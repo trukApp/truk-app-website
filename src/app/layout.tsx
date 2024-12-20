57a2692b782b246aa72de23dd9eb578a118f521c
komarajuKrishna <vamsi.komaraju@ro-one.in>
2024-12-20 12:22:50 +0530

    creating devbranch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import ReduxProvider from "@/store/redux-provider";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Truk app",
