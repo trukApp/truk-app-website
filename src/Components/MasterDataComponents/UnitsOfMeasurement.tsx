5ac7a486ed1b72e7cd36cf82dcddd0ef3b1b57ff
komarajuKrishna <vamsi.komaraju@ro-one.in>
2024-12-30 20:19:26 +0530

    uom

--- src/Components/MasterDataComponents/UnitsOfMeasurement.tsx
+++ src/Components/MasterDataComponents/UnitsOfMeasurement.tsx
@@ -1,89 +1,184 @@
-import React from 'react'
-import { DataGridComponent } from '../GridComponent';
+import React, { useState } from 'react';
+import { Formik, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { Button, TextField, Box, Grid, Collapse } from '@mui/material';
 import { GridColDef } from '@mui/x-data-grid';
-import { Grid } from '@mui/material';
+import { DataGridComponent } from '../GridComponent';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import styles from './MasterData.module.css'
 
-const weightUnitsData = [
-  { id: 1, unit: 'Kilogram', abbreviation: 'kg', equivalentInGrams: 1000 },
-  { id: 2, unit: 'Pound', abbreviation: 'lb', equivalentInGrams: 453.592 },
-  { id: 3, unit: 'Ounce', abbreviation: 'oz', equivalentInGrams: 28.3495 },
-  { id: 4, unit: 'Ton', abbreviation: 't', equivalentInGrams: 1000000 },
-  { id: 5, unit: 'Gram', abbreviation: 'g', equivalentInGrams: 1 },
-];
-
-const weightColumns: GridColDef[] = [
-  { field: 'unit', headerName: 'Unit', width: 200 },
-  { field: 'abbreviation', headerName: 'Abbreviation', width: 150 },
-  { field: 'equivalentInGrams', headerName: 'Equivalent in Grams', width: 200 },
-];
-
-
-const jewelWeightsData = [
-  { id: 1, unit: 'Carat', abbreviation: 'ct', equivalentInGrams: 0.2 },
-  { id: 2, unit: 'Milligram', abbreviation: 'mg', equivalentInGrams: 0.001 },
-  { id: 3, unit: 'Tola', abbreviation: 'tola', equivalentInGrams: 11.6638 },
-  { id: 4, unit: 'Baht', abbreviation: 'baht', equivalentInGrams: 15.244 },
-  { id: 5, unit: 'Gram', abbreviation: 'g', equivalentInGrams: 1 },
-];
-
-const jewelcolumns: GridColDef[] = [
-  { field: 'unit', headerName: 'Unit', width: 200 },
-  { field: 'abbreviation', headerName: 'Abbreviation', width: 150 },
-  { field: 'equivalentInGrams', headerName: 'Equivalent in Grams', width: 200 },
-];
-
-
-const liquidUnitsData = [
-  { id: 1, unit: 'Liter', abbreviation: 'L', equivalentInMilliliters: 1000 },
-  { id: 2, unit: 'Milliliter', abbreviation: 'mL', equivalentInMilliliters: 1 },
-  { id: 3, unit: 'Gallon (US)', abbreviation: 'gal', equivalentInMilliliters: 3785.41 },
-  { id: 4, unit: 'Pint (US)', abbreviation: 'pt', equivalentInMilliliters: 473.176 },
-  { id: 5, unit: 'Fluid Ounce (US)', abbreviation: 'fl oz', equivalentInMilliliters: 29.5735 },
-];
-
-
-const columnsForLiquids: GridColDef[] = [
-  { field: 'unit', headerName: 'Unit', width: 200 },
-  { field: 'abbreviation', headerName: 'Abbreviation', width: 150 },
-  { field: 'equivalentInMilliliters', headerName: 'Equivalent in Milliliters', width: 200 },
-];
-
-
-
-const UnitsOfMeasurement = () => {
-  return (
-    <div>
-      <Grid item xs={12} style={{ marginTop: '50px' }}>
-        <DataGridComponent
-          columns={weightColumns}
-          rows={weightUnitsData}
-          isLoading={false}
-          pageSizeOptions={[10, 20]}
-          initialPageSize={10}
-        />
-      </Grid>
-
-      <Grid item xs={12} style={{ marginTop: '50px' }}>
-        <DataGridComponent
-          columns={jewelcolumns}
-          rows={jewelWeightsData}
-          isLoading={false}
-          pageSizeOptions={[10, 20]}
-          initialPageSize={10}
-        />
-      </Grid>
-
-      <Grid item xs={12} style={{ marginTop: '50px' }}>
-        <DataGridComponent
-          columns={columnsForLiquids}
-          rows={liquidUnitsData}
-          isLoading={false}
-          pageSizeOptions={[10, 20]}
-          initialPageSize={10}
-        />
-      </Grid>
-    </div>
-  )
+interface FormValues {
+  unitName: string;
+  unitDescription: string;
+  altUnit: string;
+  altUnitDescription: string;
+  id: number;
 }
 
-export default UnitsOfMeasurement
+const UnitsDummyData: FormValues[] = [
+  {
+    id: 1,
+    unitName: 'Kilogram',
+    unitDescription: 'Weight measurement unit',
+    altUnit: 'Gram',
+    altUnitDescription: 'Smaller unit of weight',
+  },
+  {
+    id: 2,
+    unitName: 'Meter',
+    unitDescription: 'Length measurement unit',
+    altUnit: 'Centimeter',
+    altUnitDescription: 'Smaller unit of length',
+  },
+  {
+    id: 3,
+    unitName: 'Litre',
+    unitDescription: 'Volume measurement unit',
+    altUnit: 'Millilitre',
+    altUnitDescription: 'Smaller unit of volume',
+  },
+  {
+    id: 4,
+    unitName: 'Hour',
+    unitDescription: 'Time measurement unit',
+    altUnit: 'Minute',
+    altUnitDescription: 'Smaller unit of time',
+  },
+];
+
+
+const columns: GridColDef[] = [
+  { field: 'unitName', headerName: 'Unit Name', flex: 1 },
+  { field: 'unitDescription', headerName: 'Unit Description', flex: 2 },
+  { field: 'altUnit', headerName: 'Alt. Unit', flex: 1 },
+  { field: 'altUnitDescription', headerName: 'Alt. Unit Description', flex: 2 },
+];
+
+
+const UnitsOfMeasurement: React.FC = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  const validationSchema = Yup.object({
+    unitName: Yup.string().required('Unit Name is required'),
+    unitDescription: Yup.string().required('Unit Description is required'),
+    altUnit: Yup.string().required('Alt. Unit is required'),
+    altUnitDescription: Yup.string().required('Alt. Unit Description is required'),
+  });
+
+
+  const initialValues: FormValues = {
+    id: 1,
+    unitName: '',
+    unitDescription: '',
+    altUnit: '',
+    altUnitDescription: '',
+  };
+
+
+  const handleSubmit = (values: FormValues) => {
+    console.log('Form values:', values);
+  };
+
+  return (
+    <Box p={3}>
+      <Box display="flex" justifyContent="flex-end" marginBottom={3} gap={2}>
+        <Button
+          variant="contained"
+          onClick={() => setShowForm((prev) => !prev)}
+          className={styles.createButton}
+        >
+          Create Unit
+          {showForm ? <KeyboardArrowUpIcon style={{ marginLeft: 8 }} /> : <KeyboardArrowDownIcon style={{ marginLeft: 8 }} />}
+        </Button>
+      </Box>
+
+      <Collapse in={showForm}>
+        <Box marginBottom={4} padding={2} border="1px solid #ccc" borderRadius={2}>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ handleChange, handleBlur, values, touched, errors }) => (
+              <Form>
+                <Grid container spacing={2} style={{ marginBottom: '30px' }}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      fullWidth
+                      label="Unit Name*"
+                      name="unitName"
+                      value={values.unitName}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      variant="outlined"
+                      error={touched.unitName && Boolean(errors.unitName)}
+                      helperText={<ErrorMessage name="unitName" />}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      fullWidth
+                      label="Unit Description*"
+                      name="unitDescription"
+                      value={values.unitDescription}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      variant="outlined"
+                      error={touched.unitDescription && Boolean(errors.unitDescription)}
+                      helperText={<ErrorMessage name="unitDescription" />}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      fullWidth
+                      label="Alt. Unit*"
+                      name="altUnit"
+                      value={values.altUnit}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      variant="outlined"
+                      error={touched.altUnit && Boolean(errors.altUnit)}
+                      helperText={<ErrorMessage name="altUnit" />}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      fullWidth
+                      label="Alt. Unit Description*"
+                      name="altUnitDescription"
+                      value={values.altUnitDescription}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      variant="outlined"
+                      error={touched.altUnitDescription && Boolean(errors.altUnitDescription)}
+                      helperText={<ErrorMessage name="altUnitDescription" />}
+                    />
+                  </Grid>
+                </Grid>
+
+                <Box display="flex" justifyContent="center" gap={2}>
+                  <Button type="submit" variant="contained" color="primary">
+                    Submit
+                  </Button>
+                </Box>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Collapse>
+
+      <Grid item xs={12} style={{ marginTop: '70px' }}>
+        <DataGridComponent
+          columns={columns}
+          rows={UnitsDummyData}
+          isLoading={false}
+          pageSizeOptions={[10, 20]}
+          initialPageSize={10}
+        />
+      </Grid>
+    </Box>
+  );
+};
+
+export default UnitsOfMeasurement;
