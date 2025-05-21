a4c5bb4253de5acc9b18db3eca759c8c32f6db55
komarajuKrishna <vamsi.komaraju@ro-one.in>
2025-05-21 12:58:53 +0530

    redelpoyment

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -2,7 +2,8 @@ const apiconfig = {
         develpoment: {
                 // apiBaseUrl: 'https://dev-api.trukapp.com/truk/',
 
-                apiBaseUrl: 'http://13.127.36.10:8088/truk/'
+                // apiBaseUrl: 'http://13.127.36.10:8088/truk/'
+                apiBaseUrl: '/api/',
 
         },
 };
