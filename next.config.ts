0a1d6d0ba0300d3c1d1a04030ce82d0ad3636855
komarajuKrishna <vamsi.komaraju@ro-one.in>
2025-05-21 12:50:01 +0530

    redelpoyment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["trukapp.s3.ap-south-1.amazonaws.com"],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["trukapp.s3.ap-south-1.amazonaws.com"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/count-data', // frontend path
+        destination: 'http://13.127.36.10:8088/truk/data/count-data', // backend API
+      },
+    ];
   },
 };
 
